9539539f931fea2aad49f59d423718b0b3da547f
rafael_pereira4 <rafael.pereira@gruporbs.com.br>
2014-02-26 19:15:47 -0300

    finding elements and adding libs

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ var App = function() {}
 App.prototype = {
 	initialize: function() {
 		cl('App initialized!');
+		this.populateDataUrlPosts();
+	},
+
+	populateDataUrlPosts: function () {
+		var self = this,
+			posts = this.find('post');
+		
+		_.each(posts, function(item) {
+			var el = item,
+				titleEl = self.find('post_title', el)[0],
+				title = self.replaceSpaces(titleEl.innerHTML),
+				id = item['id'],
+				url = item['data_url'];
+		})
+	},
+
+	replaceSpaces: function (str) {
+		return str.split(' ').join('_');
+	},
+
+	find: function (className, element) {
+		if(element) {
+			return element.getElementsByClassName(className);
+		}
+
+		return document.getElementsByClassName(className);
 	}
 }
 
