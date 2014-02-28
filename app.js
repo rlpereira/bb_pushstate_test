b9c021491f2891e0455c49a35de3a0b4069f0f6e
rafael_pereira4 <rafael.pereira@gruporbs.com.br>
2014-02-28 15:18:37 -0300

    fininshing tests without WP

--- app.js
+++ app.js
@@ -11,19 +11,26 @@ var App = function() {}
 App.prototype = {
 	initialize: function() {
 		cl('App initialized!');
+		Backbone.history.start({ silent: true, pushState: true, root: '/bb_pushstate_test/' });
 		this.populateDataUrlPosts();
 	},
 
 	populateDataUrlPosts: function () {
 		var self = this,
-			posts = this.find('post');
+			posts = this.findByClassName('post');
 		
 		_.each(posts, function(item) {
 			var el = item,
-				titleEl = self.find('post_title', el)[0],
-				title = self.replaceSpaces(titleEl.innerHTML),
+				titleEl = self.findByClassName('post_title', el)[0],
 				id = item['id'],
 				url = item['data_url'];
+
+				$('#' + id).attr('data-url', self.replaceSpaces(titleEl.innerHTML));
+
+				$('#' + id).waypoint(function(direction) {
+					var url = $(this).attr('data-url');
+					Backbone.history.navigate(url, true);
+				});
 		})
 	},
 
@@ -31,12 +38,20 @@ App.prototype = {
 		return str.split(' ').join('_');
 	},
 
-	find: function (className, element) {
+	findByClassName: function (className, element) {
 		if(element) {
 			return element.getElementsByClassName(className);
 		}
 
 		return document.getElementsByClassName(className);
+	},
+
+	findById: function (id, element) {
+		if(element) {
+			return elemente.getElementById(id);
+		}
+
+		return document.getElementById(id);
 	}
 }
 
